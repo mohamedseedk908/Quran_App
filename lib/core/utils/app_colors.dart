184090049cdf7fee63ce46b6d6384a47978b984c
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-03-04 01:14:28 -0800

    add colors in app

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors
+{
+  static Color lavenderPinkish = Color(0xffE5B6F2);
+  static Color lightPurple = Color(0xffCD77F2);
+  static Color ofWight = Color(0xff7B15BF);
+  static Color brightPurple = Color(0xff9D1DF2);
+  static Color  deepViolet = Color(0xff300759);
+}
