75ada70247397e04946ac735cb479f2ad55813aa
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-03-04 09:29:47 -0800

    refactor code in starting screen

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -4,6 +4,7 @@ abstract class AppColors
 {
   static Color primaryColors = Color(0xffE5E5E5);
   static Color lavenderPinkish = Color(0xffE5B6F2);
+  static Color pinkish = Color(0xffE5B6F0);
   static Color lightPurple = Color(0xffCD77F2);
   static Color ofWight = Color(0xff7B15BF);
   static Color brightPurple = Color(0xff9D1DF2);
--- lib/features/starting_page/presentation/views/starting_screen.dart
+++ lib/features/starting_page/presentation/views/starting_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quran_app/core/utils/app_assets.dart';
 import 'package:quran_app/core/utils/app_colors.dart';
-import 'package:quran_app/core/utils/app_strings.dart';
-import 'package:quran_app/core/utils/custom_text_stayls.dart';
-import 'package:quran_app/features/starting_page/presentation/widgets/app_text_button.dart';
+import 'package:quran_app/features/starting_page/presentation/widgets/body_starting_screen.dart';
 
 class StartingScreen extends StatelessWidget {
   const StartingScreen({super.key});
@@ -11,40 +8,8 @@ class StartingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryColors,
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 113),
-            Text(
-              textAlign: TextAlign.center,
-              AppStrings.nameApp,
-              style: CustomTextStyles.poppinsBold24,
-            ),
-            SizedBox(
-              height: 6,
-            ),
-            Text(
-              textAlign: TextAlign.center,
-              AppStrings.supTitleInSplashScreen,
-              style: CustomTextStyles.poppinsBold13,
-            ),
-            SizedBox(
-              height: 41,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                  color: AppColors.deepViolet),
-              width: 296,
-              height: 437,
-              child: Image.asset(AppAssets.mosque),
-            ),
-            SizedBox(height: 45,),
-            CustomButton(onPressed: (){},)
-          ],
-        ),
-      ),
+      backgroundColor: AppColors.pinkish,
+      body: BodyStartingScreen(),
     );
   }
 }
